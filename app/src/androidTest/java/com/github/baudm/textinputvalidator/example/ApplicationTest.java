14cf71dfcfa423991fd3974613c40f53ff8664ec
Severin Julien <sapher974@gmail.com>
2017-04-25 11:43:32 +0400

    Update project and add instrument test

--- app/src/androidTest/java/com/github/baudm/textinputvalidator/example/ApplicationTest.java
+++ app/src/androidTest/java/com/github/baudm/textinputvalidator/example/ApplicationTest.java
@@ -1,13 +1,74 @@
 package com.github.baudm.textinputvalidator.example;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.app.Activity;
+import android.support.design.widget.TextInputLayout;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testInvalidInputs() {
+
+        final Activity activity = mActivityRule.getActivity();
+
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withText("One or more inputs invalid.")).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+
+        /*onView(withId(R.id.first_name))
+                .check(matches(hasTextInputLayoutHintText("First Name")));*/
+    }
+
+    public static Matcher<View> hasTextInputLayoutHintText(final String expectedErrorText) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextInputLayout)) {
+                    return false;
+                }
+
+                CharSequence error = ((TextInputLayout) view).getHint();
+
+                if (error == null) {
+                    return false;
+                }
+
+                String hint = error.toString();
+
+                return expectedErrorText.equals(hint);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
     }
 }
